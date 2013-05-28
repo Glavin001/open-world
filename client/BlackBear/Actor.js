9467c85192bee79b879eb024e81377035bc5fd4c
Liam Coleman-Aulenbach <liamcoau@gmail.com>
2013-05-28 19:11:34 -0300

    Adding BlackBear stuff

---
+++ client/BlackBear/Actor.js
@@ -0,0 +1,52 @@
+/************************************************
+*Root class for all classes visible in the scene*
+*TODO: Everything********************************
+************************************************/
+
+var BLACKBEAR = BLACKBEAR || new Object();
+
+BLACKBEAR.actor = BLACKBEAR.actor || new Object(BLACKBEAR.object);
+
+//////////////////////////////////////////////////////////////////
+
+BLACKBEAR.actor.lastTick;
+
+BLACKBEAR.actor.deltaTime;
+
+BLACKBEAR.actor.location;
+
+BLACKBEAR.actor.rotation;
+
+BLACKBEAR.actor.velocity;
+
+BLACKBEAR.actor.acceleration;
+
+BLACKBEAR.actor.owner;
+
+//////////////////////
+
+BLACKBEAR.actor.tick = function(deltaTime) {
+	
+};
+
+BLACKBEAR.actor.callTick = function() {
+	var currentTick = getTime();
+	
+	deltaTime = currentTick - lastTick;
+	
+	lastTick = currentTick;
+	
+	tick(deltaTime);
+};
+
+BLACKBEAR.actor.setLocation = function(newLocation) {
+	
+};
+
+BLACKBEAR.actor.setRotation = function(newRotation) {
+	
+};
+
+BLACKBEAR.actor.setOwner = function(newOwner) {
+	
+};
---
+++ client/BlackBear/Object.js
@@ -0,0 +1,12 @@
+/**********************************************************
+*Root class in the BlackBear engine, except for interfaces*
+*TODO: Everything******************************************
+**********************************************************/
+
+var BLACKBEAR = BLACKBEAR || new Object();
+
+BLACKBEAR.object = BLACKBEAR.object || new Object();
+
+////////////////////////////////////////////////////
+
+BLACKBEAR.object.date = new Date();
