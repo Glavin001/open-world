6be1a93df34d73e040f4cb40bf3184a58f0e5e5a
Scott Wesley <scott.wesley@ns.sympatico.ca>
2013-07-30 16:04:03 -0300

    Update to sound code -Tick function -Improved functionality -Using IB namespace -Improvement to panning (still not fully functional) -Early implementation and testing of background music

--- client/js/3d_sound.js
+++ client/js/3d_sound.js
@@ -1,30 +1,32 @@
 /*****************************************************************
 	UTILITY FUNCTIONS
 *****************************************************************/
-checkVolume = function(newVolume) {
-	if(newVolume > 1)
-		newVolume = 1;
-	else if(newVolume < 0)
-		newVolume = 0;
+checkRange = function(value, min, max) {
+	if(value > max)
+		value = max;
+	else if(value < min)
+		value = min;
 	
-	return newVolume;
+	return value;
 }
 
 /*****************************************************************
 	CORE SOUND OBJECT
 *****************************************************************/
 
-Sound = {};
-Sound.context = null;
-Sound.soundList = [];
-Sound.pointList = [];
-Sound.loadCount = 0;
-Sound.sfxVolume = 1;
-Sound.bgVolume = 1;
+var IB = IB || new Object();
+IB.sound = IB.sound || {};
+
+IB.sound.context = null;
+IB.sound.soundList = [];
+IB.sound.pointList = [];
+IB.sound.loadCount = 0;
+IB.sound.sfxVolume = 1;
+IB.sound.bgVolume = 1;
 
 try {
 	window.AudioContext = window.AudioContext||window.webkitAudioContext;
-	Sound.context = new AudioContext();
+	IB.sound.context = new AudioContext();
 	console.log('Web Audio API loaded');
 }
 catch(e) {
@@ -32,11 +34,11 @@ catch(e) {
 	console.log('Web Audio API failed to load');
 }
 
-Sound.tick = function() {
+IB.sound.tick = function(coordinates, angle) {
 	var length = this.pointList.length;
 	
 	for(i = 0; i < length; i++) {
-		
+		this.pointList[i].updateLevels(coordinates, angle);
 	}
 }
 
@@ -45,24 +47,23 @@ function soundRequest(url, total, callback) {
 	request.open('GET', url, true);
 	request.responseType = 'arraybuffer';
 	request.onload = function() {
-		Sound.loadCount++;
+		IB.sound.loadCount++;
 		
-		Sound.soundList[url] = Sound.context.createBufferSource();
-		Sound.context.decodeAudioData(request.response, function(buffer) {
-			Sound.soundList[url].buffer = buffer;
+		IB.sound.context.decodeAudioData(request.response, function(buffer) {
+			IB.sound.soundList[url] = buffer;
+			
+			console.log('Sound loaded:', url);
+			
+			if(IB.sound.loadCount >= total) {
+				console.log('All sounds preloaded');
+				callback();
+			}
 		});
-		
-		console.log('Sound loaded:', url);
-		
-		if(Sound.loadCount >= total) {
-			console.log('All sounds preloaded');
-			callback();
-		}
 	}
 	request.send();
 }
 
-Sound.loadSound = function(soundReq, callback) {
+IB.sound.loadSound = function(soundReq, callback) {
 	var length = soundReq.length;
 	
 	for(i = 0; i < length; i++) {
@@ -70,13 +71,16 @@ Sound.loadSound = function(soundReq, callback) {
 	}
 }
 
-Sound.setMasterVolume = function(newVolume) {
-	if(newVolume > 1)
-		newVolume = 1;
-	else if(newVolume < 0)
-		newVolume = 0;
+IB.sound.setSfxVolume = function(newVolume) {
+	newVolume = checkRange(newVolume, 0, 1);
 	
-	masterVolume = newVolume;
+	this.sfxVolume = newVolume;
+}
+
+IB.sound.setBgVolume = function(newVolume) {
+	newVolume = checkRange(newVolume, 0, 1);
+	
+	this.bgVolume = newVolume;
 }
 
 /*****************************************************************
@@ -84,10 +88,10 @@ Sound.setMasterVolume = function(newVolume) {
 *****************************************************************/
 
 function Sound_Reg(sound_url, canLoop) {
-	//Creates variables for source and nodes
-	this.sound = Sound.soundList[sound_url];
-	this.volume = Sound.context.createGain();
-	this.pan = Sound.context.createPanner();
+	this.sound;
+	this.sound_url = sound_url;
+	this.volume = IB.sound.context.createGain();
+	this.pan = IB.sound.context.createPanner();
 	
 	if(canLoop === undefined)
 		canLoop = false;
@@ -96,63 +100,36 @@ function Sound_Reg(sound_url, canLoop) {
 }
 
 Sound_Reg.prototype.create = function() {
-	//Connects nodes
+	this.sound = IB.sound.context.createBufferSource();
+	this.sound.buffer = IB.sound.soundList[this.sound_url];
 	this.sound.connect(this.volume);
 	this.sound.loop = this.canLoop;
 	this.volume.connect(this.pan);
-	this.pan.connect(Sound.context.destination);
+	this.pan.connect(IB.sound.context.destination);
 }
 
-Sound_Reg.prototype.play = function(delay) {
-	var time;
-	
-	if(delay === undefined) {
-		time = 0;
-	} else {
-		time = delay / 1000;
-	}
-	
-	this.sound.noteOn(time);
+Sound_Reg.prototype.play = function() {
+	this.sound.noteOn(0);
 };
 
-Sound_Reg.prototype.pause = function(delay) {
-	var time;
-	
-	if(delay === undefined) {
-		time = 0;
-	} else {
-		time = delay / 1000;
-	}
-	
-	this.sound.noteOff(time);
+Sound_Reg.prototype.stop = function() {
+	this.sound.noteOff(0);
 };
 
 Sound_Reg.prototype.setPan = function(pan_x, pan_y, pan_z) {
-	if(pan_x > 1)
-		pan_x = 1;
-	else if (pan_x < -1)
-		pan_x = -1;
+	pan_x = checkRange(pan_x, -1, 1);
 	
-	if(pan_y > 1)
-		pan_y = 1;
-	else if (pan_y < -1)
-		pan_y = -1;
+	pan_y = checkRange(pan_y, -1, 1);
 	
-	if(pan_z > 1)
-		pan_y = 1;
-	else if (pan_x < -1)
-		pan_z = -1;
+	pan_z = checkRange(pan_z, -1, 1);
 	
 	this.pan.setPosition(pan_x, pan_y, pan_z);
 };
 
 Sound_Reg.prototype.setVolume = function(newVolume) {
-	if(newVolume > 1)
-		newVolume = 1;
-	else if(newVolume < 0)
-		newVolume = 0;
+	newVolume = checkRange(newVolume, 0, 1);
 	
-	newVolume = newVolume * Sound.sfxVolume;
+	newVolume = newVolume * IB.sound.sfxVolume;
 	
 	this.volume.gain.value = newVolume;
 };
@@ -160,30 +137,49 @@ Sound_Reg.prototype.setVolume = function(newVolume) {
 Sound_Reg.prototype.settings = function() {
 };
 
+IB.sound.createSound = function(sound_url, canLoop) {
+	var temp = new Sound_Reg(sound_url, canLoop);
+	
+	temp.create();
+	
+	return temp;
+}
+
 /*****************************************************************
 	3D SOUND OBJECT
 *****************************************************************/
 
-function Sound_3d(sound_url, location, reference_intensity, reference_distance, canLoop, position) {
-	//Creates variables to control location and intensity
+function deletePointSound(object) {
+	var array = Sound.pointList;
+	var index = object.index;
+	console.log(index);
+	var limit = array - 1;
+	
+	for(i = index; i < limit; i++) {
+		array[i] = array[i + 1]
+		array[i].index = i;
+	}
+	
+	array.pop();
+}
+
+function Sound_3d(sound_url, location, reference_intensity, reference_distance, canLoop) {
 	this.x = location.x;
 	this.y = location.y;
 	this.z = location.z;
+	this.index;
 	this.reference_intensity = reference_intensity;
 	this.reference_distance = reference_distance;
-	this.sound = Sound.soundList[sound_url];
-	this.position = position;
+	this.sound;
+	this.sound_url = sound_url;
 	
 	if(canLoop === undefined)
 		canLoop = false;
-		
-	this.sound.loop = canLoop;
 }
 
 Sound_3d.prototype = new Sound_Reg();
 
 Sound_3d.prototype.updateLevels = function(current, theta) {
-	//Calculates volume based on distance
 	var distance = Math.sqrt( Math.pow(current.x - this.x, 2) + Math.pow(current.y - this.y, 2) + Math.pow(current.z - this.z, 2) );
 	
 	if(distance < 1)
@@ -191,62 +187,54 @@ Sound_3d.prototype.updateLevels = function(current, theta) {
 		
 	var newVolume = Math.pow(this.reference_distance / distance, 2) * this.reference_intensity;
 	
-	//Pans the sound based on angle from sound source to player
-	var cos_angle;
-	var sin_angle;
+	var pan_x;
+	var pan_y;
 	var mod_x = this.x - current.x;
+	var mod_y = this.y - current.y;
 	var mod_z = this.z - current.z;
 	var radius =  Math.sqrt( Math.pow(mod_x, 2) + Math.pow(mod_z, 2) );
 	
 	if(radius === 0) {
-		cos_angle = 0;
-		sin_angle = 0;
+		pan_x = 0;
+		pan_y = 0;
+		pan_z = 0;
 	} else {
-		cos_angle = mod_x / radius;
-		sin_angle = mod_z / radius;
+		pan_x = mod_x / radius;
+		pan_y = mod_z / radius;
+		pan_z = mod_y / radius;
 	}
 	
-	var pan_x = -Math.cos( Math.acos(cos_angle) + theta );
-	var pan_z = -Math.sin( Math.asin(sin_angle) + theta );
-	
-	//Sets the volume and pan of sound
 	this.setVolume(newVolume);
-	this.setPan(pan_x, 0, pan_z);
+	this.setPan(pan_x, pan_y, pan_z);
 };
 
-function createSound3D(sound_url, location, reference_intensity, reference_distance, canLoop) {
-	var length = Sound.pointList.length;
+IB.sound.createSound3D = function(sound_url, location, reference_intensity, reference_distance, canLoop) {
+	IB.sound.pointList[length] = new Sound_3d(sound_url, location, reference_intensity, reference_distance, canLoop);
 	
-	//Adds a new Sound_3d object to the list of Sound_3d objects
-	Sound.pointList[length] = new Sound_3d(sound_url, location, reference_intensity, reference_distance, canLoop, length);
-	
-	//Sets up and activates the Sound_3d
-	Sound.pointList[length].create();
-	Sound.pointList[length].play(0);
+	IB.sound.pointList[length].index = length;
+	IB.sound.pointList[length].create();
+	IB.sound.pointList[length].play(0);
 }
 
 /*****************************************************************
 	BACKGROUND SOUND OBJECT
 *****************************************************************/
 
-Background_Music = {};
-Background_Music.prototype = new Sound_Reg();
-Background_Music.isRandom = true;
-Background_Music.count = 0;
-Background_Music.playlist = [];
+IB.background_music = IB.background_music || {};
+IB.background_music.prototype = new Sound_Reg();
+IB.background_music.isRandom = true;
+IB.background_music.count = 0;
+IB.background_music.playlist = [];
 
-Background_Music.setVolume = function(newVolume) {
-	if(newVolume > 1)
-		newVolume = 1;
-	else if(newVolume < 0)
-		newVolume = 0;
+IB.background_music.setVolume = function(newVolume) {
+	newVolume = checkRange(newVolume, 0, 1);
 	
-	newVolume = newVolume * Sound.bgVolume;
+	newVolume = newVolume * IB.sound.bgVolume;
 	
 	this.volume.gain.value = newVolume;
 };
 
-Background_Music.loadPlaylist = function(songNames, setRandom) {
+IB.background_music.loadPlaylist = function(songNames, setRandom) {
 	var length = songNames.length;
 	this.playlist = [];
 	
@@ -256,9 +244,29 @@ Background_Music.loadPlaylist = function(songNames, setRandom) {
 		this.isRandom = setRandom;
 	
 	for(i = 0; i < length; i++) {
-		this.playlist[i] = Sound.soundList[songNames[i]];
+		this.playlist[i] = IB.sound.soundList[songNames[i]];
 	}
 }
 
-Background_Music.selectSong = function() {
+IB.background_music.selectSong = function() {
+	var length = this.playlist.length;
+	var selection;
+	
+	if(this.isRandom) {		
+		selection = Math.ceil(Math.random() * length) - 1;
+		
+		if(selection === this.count)
+			selection++;
+	} else {
+		selection = count++;
+	}
+	
+	if(selection > this.playlist.length - 1)
+		selection = 0;
+	
+	return selection;
+}
+
+IB.background_music.forceChange = function() {
+	this.count = this.selectSong();
 }
