b4f395cc544c9ee72a017009f3c3ffded4e5fab2
Liam Coleman-Aulenbach <liamcoau@gmail.com>
2013-08-11 00:42:42 -0300

    bug fix

--- client/OpenWorld/Network.js
+++ client/OpenWorld/Network.js
@@ -12,10 +12,9 @@ OW.subEngines.push(OW.network);
 
 OW.network.connectToServer = function() {
 	this.__proto__.connectToServer.apply(this);
-	console.log(this.server);
+
 	this.server.on("connect", function () {
-		console.log(this.server);
-		this.server.emit("init", {id: OW.player.id, username: OW.player.username, willServe: true});
+		OW.network.server.emit("init", {id: OW.player.id, username: OW.player.username, willServe: true});
 	});
 	this.server.on("init", function (data) {console.log(data);});
 };
